8f7e0edf1269fc2ef798b74dfd7eb345f6d55e16
saravana <saravanap107@gmail.com>
2026-08-14 23:57:22 +0530

    added if stage

--- rtl/core/coretop.v
+++ rtl/core/coretop.v
@@ -42,4 +42,24 @@ parameter RR_ALU = 3'b000,
 reg HALTED;
 reg TAKEN_BRANCH;
 
+
+always @(posedge clk1)              // IF Stage
+    if (HALTED == 0)
+    begin
+        if ((((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) ||
+             ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0))))
+        begin
+            IF_ID_IR <= #2 Mem[EX_MEM_ALUout];
+            TAKEN_BRANCH <= #2 1'b1;
+            IF_ID_NPC <= #2 EX_MEM_ALUout + 1;
+            PC <= #2 EX_MEM_ALUout + 1;
+        end
+        else
+        begin
+            IF_ID_IR <= #2 Mem[PC];
+            IF_ID_NPC <= #2 PC + 1;
+            PC <= #2 PC + 1;
+        end
+    end
+
 endmodule
